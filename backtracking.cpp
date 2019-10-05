a752094642d32f03ad9c82450f9ee800d44128a3
Daniel Henrique Ferreira Gomes <danielhenriquefg@gmail.com>
2019-10-05 14:53:48 -0300

    corigindo o backtracking

--- backtracking.cpp
+++ backtracking.cpp
@@ -28,68 +28,86 @@ int overlap(std::string a, std::string b, int min_length=5)
 
 void backtranckingSCS(std::vector<std::string> a, size_t n,
             size_t depth,std::vector<bool> used, std::vector<std::string> curr, 
-            std::string stringThatLevel, std::string * shortest){
+            std::string stringThatLevel, std::string * shortest, int * iter){
     
     if (depth == n){
         //std::vector<bool>::iterator it = std::find(used.begin(), used.end(), 0);
         //int index = std::distance(used.begin(), it);
         //int over = overlap(stringThatLevel, a[index], 1);
         //stringThatLevel += a[index].substr(over);
-        for (auto const& c: used) {
-            std::cout << c << " ";
-        }
-        std::cout << std::endl;
-        int over = overlap(stringThatLevel, curr[n-1], 1);
+        //int over = overlap(stringThatLevel, curr[n-1], 1);
+        /*
+        std::cout << over;
         stringThatLevel += curr[n-1].substr(over);
         if (*shortest != "" and stringThatLevel.size() >= shortest->size()){
             return;
         }
         else
             *shortest = stringThatLevel;
+        std::cout << over << "A";*/
+        *shortest = stringThatLevel;
+        std::cout << "Tamanho da menor superstring atual: " << shortest->size() << std::endl;
     }
     
     for (int i = 0; i < n; ++i)
     {
         if (not used[i])
         {  
+            *iter += 1;
+            curr.push_back(a[i]);
+            used[i] = true;
+            for (auto const& c: curr) {
+                if (c == curr[curr.size()-1])
+                {
+                    std::cout << c;
+                }
+                else
+                    std::cout << "|-";
+            }
+            std::cout << std::endl;
+
+
             std::string stringAux = "";
             if (stringThatLevel == "")
             {
                 stringAux = a[i];
+                backtranckingSCS(a, n, depth+1, used, curr, stringAux, shortest, iter);
+                curr.pop_back();
+                //std::cout << "Voltando: ";
+                //for (auto const& c: curr) {
+                    //std::cout << c << " ";
+                //}
+                //std::cout << std::endl;
+                used[i] = false;
             }
             else{
                 stringAux = stringThatLevel;
                 int over = overlap(stringThatLevel, a[i], 1);
                 stringAux += a[i].substr(over);
                 //stringThatLevel = stringThatLevel;
-                if (*shortest != "" and stringAux.size() >= shortest->size()){
-                    return;
+                if (not (*shortest != "") or stringAux.size() < shortest->size()){
+                    backtranckingSCS(a, n, depth+1, used, curr, stringAux, shortest, iter);
+                    //std::cout << "Voltando: ";
+                    //for (auto const& c: curr) {
+                        //std::cout << c << " ";
+                    //}
+                    //std::cout << std::endl;
                 }
+                curr.pop_back();
+                used[i] = false;
+
             }
 
-
-            curr.push_back(a[i]);
-            used[i] = true;
-            for (auto const& c: curr) {
-                std::cout << c << " ";
-            }
-            std::cout << std::endl;
-            backtranckingSCS(a, n, depth+1, used, curr, stringAux, shortest);
-            curr.pop_back();
-            std::cout << "Voltando: ";
-            for (auto const& c: curr) {
-                std::cout << c << " ";
-            }
-            std::cout << std::endl;
-            used[i] = false;
         }
     }
     return;
 }
 
-int main()
+int main(int argc, char *argv[])
 {
-    std::vector<std::string> a = {"ccd", "bbc", "abb", "ddddddd"};
+    std::vector<std::string> a = {"bbbbbbbcdd", "bbbbbbbbcd", "abbbbbbbb", "ddddddd"};
+    //std::vector<std::string> a = {"ccd", "bbc", "abb", "ddddddd"};
+    //std::vector<std::string> a = {"abb", "ccddddd", "bbc"};
     auto n(a.size());
     std::vector<bool> used;
     for (size_t i = 0; i < n; ++i)
@@ -100,7 +118,11 @@ int main()
     std::string * shortest = new std::string("");
     //*shortest = "";
     //std::cout << *shortest;
-    backtranckingSCS(a, n, 0, used, curr, "", shortest);
+    int * iter = new int();
+    *iter = 0;
+    backtranckingSCS(a, n, 0, used, curr, "", shortest, iter);
     std::cout << "Shortest: "<< *shortest << std::endl;
+    std::cout << "Iterações: "<< *iter << std::endl;
+
     return 0;
 }
